08211ac25895f1ea1a523a1d8236727983ade5ee
JacobDChamberlain <straightlegmusic@gmail.com>
2023-09-26 23:39:48 -0500

    temp page routes, begin nav bar

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Auth from '../Auth/Auth';
+import NavigationBar from '../NavigationBar/NavigationBar';
+
 
 function App() {
   return (
-    <>
-      <Auth />
-    </>
+    <div className='app-wrapper'>
+      <NavigationBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' Component={ Auth } />
+          {/* <Route path='/' Component={  } />
+          <Route path='/tabs' Component={  } />
+          <Route path='/tabs/:tabId' Component={  } /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ frontend/src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function NavigationBar() {
+    return(
+        <div className='navigation-bar-wrapper'>
+            <h2>Welcome, User!</h2>
+        </div>
+    )
+}

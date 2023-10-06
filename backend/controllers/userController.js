9564562eee76ba0c6f5dac47237dd82f62574413
JacobDChamberlain <straightlegmusic@gmail.com>
2023-10-06 12:08:06 -0500

    sign up route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,10 +3,48 @@ const jwt = require('jsonwebtoken');
 const db = require('../models');
 
 
-const User = db.User;
+const User = db.User; // change to Users if using line 43 of 'models/index.js'
 
 const signup = async ( req, res ) => {
-    console.log('signup');
+    try {
+        const { username, email, password } = req.body;
+        const data = {
+            username,
+            email,
+            password: await bcrypt.hash( password, 10 )
+        };
+
+        const user = await User.create( data );
+
+        if ( user ) {
+            let token = jwt.sign({ id: user.id }, process.env.secretKey, {
+                expiresIn: 1000 * 60 * 60 * 24
+            });
+
+            // do we need this if we're sending a token to the frontend?
+            // res.cookie('jwt', token, { maxAge: 60 * 60 * 24, httpOnly: true });
+
+            console.log( 'user: ', JSON.stringify( user, null, 2 ));
+            console.log( 'token: ', token );
+
+            const userData = {
+                id: user.id,
+                username: user.username,
+            };
+
+            // possibly send this in response to place user info in localstorage?
+            // const userAndTokenData = {
+            //     tokenData: token,
+            //     userData
+            // };
+
+            return res.status( 200 ).send( token );
+        } else {
+            return res.status( 409 ).send( 'Failed to sign up' );
+        }
+    } catch( err ) {
+        console.log( err );
+    }
 }
 
 const login = async ( req, res ) => {

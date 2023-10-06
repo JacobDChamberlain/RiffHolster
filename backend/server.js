a0348eb124a3011abe899f225e02ebbea0d79597
JacobDChamberlain <straightlegmusic@gmail.com>
2023-10-05 21:42:04 -0500

    setup sequelize + User Model & Migration

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const sequelize = require('sequelize');
+const cors = require('cors');
 const dotenv = require('dotenv').config();
 const cookierParser = require('cookie-parser');
+// const db = require('./models');
 
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use( cors() );
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookierParser());

edce6f66bbd28f20600d6fdb7fff6ec02847085a
JacobDChamberlain <straightlegmusic@gmail.com>
2024-04-10 17:06:20 -0500

    logout button + style , no functionality

--- frontend/src/components/NavigationBar/NavigationBar.js
+++ frontend/src/components/NavigationBar/NavigationBar.js
@@ -6,6 +6,7 @@ export default function NavigationBar() {
     return(
         <div className='navigation-bar-wrapper'>
             <h2>Welcome, User!</h2>
+            <button className='logout-button'>Log Out</button>
         </div>
     )
 }

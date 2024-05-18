114aff1e36dd472d6181d5afaf19b7d5dc3cb38e
JacobDChamberlain <straightlegmusic@gmail.com>
2024-05-18 01:10:13 -0500

    update user model to handle validation with sequelize

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { Model } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -17,22 +18,49 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       validate: {
         max: 20,
-        notNull: true,
-        notEmpty: true,
+        notNull: {
+          msg: 'Username is required'
+        },
+        notEmpty: {
+          msg: 'Username cannot be blank'
+        },
       }
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        max: 50,
-        notNull: true,
-        notEmpty: true
+        len: {
+          args: [1, 50],
+          msg: 'Username cannot be more than 50 characters'
+        },
+        notNull: {
+          msg: 'Email is required'
+        },
+        notEmpty: {
+          msg: 'Email cannot be blank'
+        },
+        isEmail: {
+          msg: 'Invalid email quit playing'
+        }
       }
     },
     hashedPassword: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Password is required'
+        },
+        notEmpty: {
+          msg: 'Password cannot be blank'
+        },
+        // validatePassword() {
+        //   if (!this.password) {
+        //     throw new Error('Password cannot be blank');
+        //   }
+        // }
+      }
     }
   }, {
     sequelize,

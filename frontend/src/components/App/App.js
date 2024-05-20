0770cba0ede251c21526f8db984fa485053f3d4c
JacobDChamberlain <straightlegmusic@gmail.com>
2024-05-20 01:18:11 -0500

    boilerplate for alphaTab

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useRef } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Auth from '../Auth/Auth';
-import NavigationBar from '../NavigationBar/NavigationBar';
-import Home from '../Home/Home';
-import useToken from './useToken';
-import useUser from './useUser';
+import React, { useEffect, useRef, useState } from 'react';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Auth from '../Auth/Auth';
+// import NavigationBar from '../NavigationBar/NavigationBar';
+// import Home from '../Home/Home';
+// import useToken from './useToken';
+// import useUser from './useUser';
 import { AlphaTabApi, Settings } from '@coderline/alphatab';
+import './App.css';
 
 
 function App() {
   const elementRef = useRef( null );
+  const [api, setApi] = useState();
 
   useEffect(() => {
     const settings = {
@@ -27,8 +29,14 @@ function App() {
 
     const api = new AlphaTabApi( elementRef.current, settings );
 
+    setApi( api );
+
     return () => api.destroy();
   }, []);
+
+  function playPause() {
+    api?.playPause();
+  }
 
   // const { token, setToken, removeToken } = useToken();
   // const { user, setUser, removeUser } = useUser();
@@ -38,7 +46,11 @@ function App() {
   // }
 
   return (
-    <div ref={ elementRef }></div>
+    <div className='app-wrapper'>
+      Hello!
+      <button onClick={() => playPause()}>Play/Pause</button>
+      <div style={{ height: '500px', border: '1px solid red', borderRadius: '40px'}} ref={elementRef}></div>
+    </div>
 
     // <div className='app-wrapper'>
     //   <NavigationBar user={ user } removeToken={ removeToken } removeUser={ removeUser } />

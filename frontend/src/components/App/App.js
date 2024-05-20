4edf4b85e4eced6b25bb7cf5d50a57256135b705
JacobDChamberlain <straightlegmusic@gmail.com>
2024-05-20 00:24:06 -0500

    installed alphaTab and added Bravura font

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,31 +1,55 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Auth from '../Auth/Auth';
 import NavigationBar from '../NavigationBar/NavigationBar';
 import Home from '../Home/Home';
 import useToken from './useToken';
 import useUser from './useUser';
+import { AlphaTabApi, Settings } from '@coderline/alphatab';
 
 
 function App() {
-  const { token, setToken, removeToken } = useToken();
-  const { user, setUser, removeUser } = useUser();
+  const elementRef = useRef( null );
 
-  if ( !token ) {
-    return <Auth setToken={ setToken } setUser={ setUser } />
-  }
+  useEffect(() => {
+    const settings = {
+      core: {
+        file: 'https://www.alphatab.net/files/canon.gp',
+        fontDirectory: '/fonts/'
+      },
+      player: {
+        enablePlayer: true,
+        enableCursor: true,
+        enableUserInteraction: true,
+        soundFont: '/soundfont/sonivox.sf2'
+      }
+    };
+
+    const api = new AlphaTabApi( elementRef.current, settings );
+
+    return () => api.destroy();
+  }, []);
+
+  // const { token, setToken, removeToken } = useToken();
+  // const { user, setUser, removeUser } = useUser();
+
+  // if ( !token ) {
+  //   return <Auth setToken={ setToken } setUser={ setUser } />
+  // }
 
   return (
-    <div className='app-wrapper'>
-      <NavigationBar user={ user } removeToken={ removeToken } removeUser={ removeUser } />
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' Component={ Home } />
-          {/* <Route path='/tabs' Component={  } />
-          <Route path='/tabs/:tabId' Component={  } /> */}
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <div ref={ elementRef }></div>
+
+    // <div className='app-wrapper'>
+    //   <NavigationBar user={ user } removeToken={ removeToken } removeUser={ removeUser } />
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route path='/' Component={ Home } />
+    //       {/* <Route path='/tabs' Component={  } />
+    //       <Route path='/tabs/:tabId' Component={  } /> */}
+    //     </Routes>
+    //   </BrowserRouter>
+    // </div>
   );
 }
 

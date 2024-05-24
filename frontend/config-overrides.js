b3b381e0ad457e8b6d857c8ded601ef4914bc2ce
JacobDChamberlain <straightlegmusic@gmail.com>
2024-05-24 15:19:23 -0500

    cleaned up config overrides

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,23 +1,8 @@
-const path = require('path')
 const { AlphaTabWebPackPlugin } = require('@coderline/alphatab/webpack');
 const { addWebpackPlugin, override } = require('customize-cra');
 
 module.exports = override(
     addWebpackPlugin(
-        new AlphaTabWebPackPlugin({
-            alphaTabSourceDir: path.resolve('./node_modules/@coderline/alphatab/dist')
-        })
+        new AlphaTabWebPackPlugin()
     )
 );
-
-// module.exports = function override(config, env) {
-//     if ( !config.plugins ) {
-//         config.plugins = [];
-//     }
-
-//     config.plugins.push( new AlphaTabWebPackPlugin({
-//         alphaTabSourceDir: path.resolve('./node_modules/@coderline/alphatab/dist')
-//     }));
-
-//     return config;
-// }
